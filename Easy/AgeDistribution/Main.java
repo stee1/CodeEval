bcbd91eef919aa34969e9d4cdd757e38686a4403
Dmitriy <krdima92@gmail.com>
2015-05-12 10:31:26 +0300

    Create Main.java

---
+++ Easy/AgeDistribution/Main.java
@@ -0,0 +1,36 @@
+
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        File file = new File(args[0]);
+        BufferedReader buffer = new BufferedReader(new FileReader(file));
+        String line;
+        while ((line = buffer.readLine()) != null) {
+            line = line.trim();
+
+            short age = Short.valueOf(line);
+
+            if (age >= 0 && age <= 2) {
+                System.out.println("Still in Mama's arms");
+            } else if (age >= 3 && age <= 4) {
+                System.out.println("Preschool Maniac");
+            } else if (age >= 5 && age <= 11) {
+                System.out.println("Elementary school");
+            } else if (age >= 12 && age <= 14) {
+                System.out.println("Middle school");
+            } else if (age >= 15 && age <= 18) {
+                System.out.println("High school");
+            } else if (age >= 19 && age <= 21) {
+                System.out.println("College");
+            } else if (age >= 23 && age <= 65) {
+                System.out.println("Working for the man");
+            } else if (age >= 66 && age <= 100) {
+                System.out.println("The Golden Years");
+            } else {
+                System.out.println("This program is for humans");
+            }
+        }
+    }
+}
